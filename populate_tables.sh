8272e9bfc367f9c8ae0fa0f3cc7fdfdd9984635f
Shahzeb <shahzebqazi1996@gmail.com>
2021-11-05 09:19:38 -0400

    corrected error in populate_tables

--- populate_tables.sh
+++ populate_tables.sh
@@ -2,7 +2,6 @@
 #export LD_LIBRARY_PATH=/usr/lib/oracle/12.1/client64/lib
 sqlplus64 "snizam/04054152@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))" <<EOF
 
-
 --Event
 INSERT INTO event (event_id, instructor, event_date, event_time, name_of_event, vacancies, event_location) 
 VALUES (12346, 'Sam', '2021-10-09', 5, 'weight lifting', 10, 'Room6');
@@ -40,6 +39,15 @@ VALUES (2, 'Jacob', 'Rokhvarg', '2001-08-15', 20, 'healthy', 6, 165);
 INSERT INTO personal_information (user_id, first_name, last_name, birthdate, age, health_condition, height, weight)
 VALUES (3, 'Shahzeb', 'Jones', '2001-08-13', 20, 'healthy', 6, 168);
 
+INSERT INTO schedule (user_id, event_id)
+VALUES(1, 12345);
+
+INSERT INTO schedule (user_id, event_id)
+VALUES(2, 12345);
+
+INSERT INTO schedule (user_id, event_id)
+VALUES(3, 12346);
+
 --Instructors
 INSERT INTO personal_information (user_id, first_name, last_name, birthdate, age, health_condition, height, weight)
 VALUES (4444, 'Sam', 'Jones', '1998-08-11', 33, 'healthy', 6, 170);
@@ -50,16 +58,6 @@ VALUES (5555, 'Dan', 'James', '1999-04-19', 44, 'healthy', 6, 165);
 INSERT INTO personal_information (user_id, first_name, last_name, birthdate, age, health_condition, height, weight)
 VALUES (6666, 'Rob', 'Lee', '1975-03-18', 55, 'healthy', 6, 168);
 
-/*Damoon, Jacob, and Shahzeb have all signed up for a class*/
-INSERT INTO schedule (user_id, event_id)
-VALUES(1, 12345);
-
-INSERT INTO schedule (user_id, event_id)
-VALUES(2, 12345);
-
-INSERT INTO schedule (user_id, event_id)
-VALUES(3, 12346);
-
 INSERT INTO instructor(instructor_id, instructor_payment_id, instructor_event_id)
 VALUES(4444, 3, 12346);
 
@@ -68,6 +66,8 @@ VALUES(5555, 4, 12345);
 
 INSERT INTO instructor(instructor_id, instructor_payment_id, instructor_event_id)
 VALUES(6666, 5, 12344);
+
+
 
 exit;
 EOF
