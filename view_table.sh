6bd9be4371238ca75c5e84fd1d41846e4fcfae1b
Shahzeb <shahzebqazi1996@gmail.com>
2021-11-05 07:56:13 -0400

    corrected error in views

--- view_table.sh
+++ view_table.sh
@@ -3,7 +3,7 @@
 sqlplus64 "snizam/04054152@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))" <<EOF
 
 create VIEW "event_view" AS select * FROM event;
-select * FROM "even_view";
+select * FROM "event_view";
 
 exit;
 EOF
