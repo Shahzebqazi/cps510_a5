5544321d3e5c1417c044c3eecd34edf0bce8a6dc
Shahzeb Qazi <shahzebqazi1996@gmail.com>
2021-12-01 09:24:58 -0500

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 'Off');
 // Create connection to Oracle
-$conn = oci_connect('snizam','04054152','(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))');
+$conn = oci_connect('username','password','(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))');
 if (!$conn) {
    $m = oci_error();
    echo $m['message'];
